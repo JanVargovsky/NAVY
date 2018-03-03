6ad3da747fb880e265a6320141e82784a951fee6
Jan Vargovský <jan.vargovsky@gmail.com>
2018-03-03 16:17:25 +0100

    backpropag with bias, weights (and bias) is recalculated right after obtaining error.

--- NAVY.Lesson3/XorNeuralNet.cs
+++ NAVY.Lesson3/XorNeuralNet.cs
@@ -22,6 +22,11 @@ namespace NAVY.Lesson3
             net0 = new Perceptron(new[] { w1, w3 }, b0);
             net1 = new Perceptron(new[] { w2, w4 }, b1);
             output = new OutputPerceptron(new[] { w5, w6 }, b2);
+
+            //var r = new Random(42);
+            //net0 = new Perceptron(new[] { r.NextDouble(), r.NextDouble() }, r.NextDouble());
+            //net1 = new Perceptron(new[] { r.NextDouble(), r.NextDouble() }, r.NextDouble());
+            //output = new OutputPerceptron(new[] { r.NextDouble(), r.NextDouble() }, r.NextDouble());
         }
 
         public double Evaluate(double[] input)
@@ -47,15 +52,18 @@ namespace NAVY.Lesson3
             // output layer
             var E = y - z;
             var dZ = L * E;
+            // adjust weights
+            output.W[0] += dZ * o0; // w5'
+            output.W[1] += dZ * o1; // w6'
+            output.B += dZ; // b2'
+
             // hidden layer
             var E1 = Perceptron.Derivative(o0) * dZ * output.W[0];
             var E2 = Perceptron.Derivative(o1) * dZ * output.W[1];
-
             // adjust weights
-            // outut layer
-            output.W[0] += dZ * o0; // w5'
-            output.W[1] += dZ * o1; // w6'
-            // hidden layer
+            // b0, b1
+            net0.B += E1; // b0'
+            net1.B += E2; // b1'
             // w1, w3, w2, w4
             net0.W[0] += x[0] * E1; // w1'
             net0.W[1] += x[1] * E1; // w3'
