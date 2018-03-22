ef0bfb56393a520490f7bcd323834768f75beaa5
Jan Vargovský <jan.vargovsky@gmail.com>
2018-03-22 21:39:32 +0100

    Lesson6 - L-system
    KochSnowFlake
    KochCurve
    QuadraticFlake
    SierpinskiTriangle
    SierpinskiArrowheadTriangle

---
+++ NAVY.Lesson6/LSystem.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NAVY.Lesson6
+{
+    public class LSystem
+    {
+        readonly string axiom;
+        readonly IDictionary<char, string> rules;
+        readonly double shift;
+        readonly double angle;
+
+        public LSystem(string axiom, IDictionary<char, string> rules, double shift, double angle)
+        {
+            this.axiom = axiom;
+            this.rules = rules;
+            this.shift = shift;
+            this.angle = angle;
+        }
+
+        enum Direction
+        {
+            Left, Right
+        }
+
+        public IEnumerable<(double X, double Y)> GetPoints(double sx, double sy, double angle, int iterations)
+        {
+            var p = (X: sx, Y: sy);
+
+            (double X, double Y) Next()
+            {
+                double Rad(double a)
+                {
+                    return (Math.PI / 180) * a;
+                }
+
+                double x = p.X + shift * Math.Cos(Rad(angle));
+                double y = p.Y + shift * Math.Sin(Rad(angle));
+                return (x, y);
+            }
+
+            void Rotate(Direction d)
+            {
+                if (d == Direction.Left)
+                    angle += this.angle;
+                else
+                    angle -= this.angle;
+            }
+
+            var rewritingRule = new List<char>(axiom);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var l = new List<char>();
+                foreach (var c in rewritingRule)
+                {
+                    if (rules.TryGetValue(c, out var rule))
+                        l.AddRange(rule);
+                    else
+                        l.Add(c);
+                }
+                rewritingRule = l;
+            }
+
+            yield return p;
+            foreach (var c in rewritingRule)
+            {
+                if (c == '+')
+                    Rotate(Direction.Left);
+                else if (c == '-')
+                    Rotate(Direction.Right);
+                else
+                {
+                    var next = Next();
+                    yield return next;
+                    p = next;
+                }
+            }
+        }
+    }
+}
---
+++ NAVY.Lesson6/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace NAVY.Lesson6
+{
+    public partial class MainWindow : Window
+    {
+        public int Iterations { get; set; }
+
+        public MainWindow()
+        {
+            Iterations = 1;
+            InitializeComponent();
+        }
+
+        void NumbersOnlyValidation(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        void ScaleAndInvertY(Point[] points, Size size)
+        {
+            var minX = points.Min(t => t.X);
+            var maxX = points.Max(t => t.X);
+            var minY = points.Min(t => t.Y);
+            var maxY = points.Max(t => t.Y);
+
+            var sizeX = maxX - minX;
+            var sizeY = maxY - minY;
+
+            var scaleX = size.Width / sizeX;
+            var scaleY = size.Height / sizeY;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].X -= minX;
+                points[i].X *= scaleX;
+                points[i].Y -= minY;
+                points[i].Y *= scaleY;
+                points[i].Y = size.Height - points[i].Y;
+            }
+        }
+
+        void AddPointsToCanvas(IEnumerable<(double X, double Y)> p)
+        {
+            canvas.Children.Clear();
+
+            var points = p.Select(t => new Point(t.X, t.Y)).ToArray();
+            ScaleAndInvertY(points, canvas.RenderSize);
+
+            var pointCollection = new PointCollection(points);
+            var lines = new Polyline
+            {
+                Points = pointCollection,
+                Stroke = Brushes.Black,
+            };
+
+            canvas.Children.Add(lines);
+        }
+
+        void AddPointsToCanvas(LSystem lSystem)
+        {
+            AddPointsToCanvas(lSystem.GetPoints(canvas.ActualWidth / 2, canvas.ActualHeight / 2, 0, Iterations));
+        }
+
+        void KochSnowFlake(object sender, RoutedEventArgs e)
+        {
+            const string Axiom = "F--F--F";
+            const string F = "F+F--F+F";
+            var l = new LSystem(Axiom, new Dictionary<char, string>
+            {
+                ['F'] = F,
+            }, 20, 60);
+            AddPointsToCanvas(l);
+        }
+
+        void KochCurve(object sender, RoutedEventArgs e)
+        {
+            const string Axiom = "F";
+            const string F = "F+F-F-F+F";
+            var l = new LSystem(Axiom, new Dictionary<char, string>
+            {
+                ['F'] = F,
+            }, 20, 90);
+            AddPointsToCanvas(l);
+        }
+
+        void QuadraticFlake(object sender, RoutedEventArgs e)
+        {
+            const string Axiom = "F+F+F+F";
+            const string F = "F+F-F-FF+F+F-F";
+            var l = new LSystem(Axiom, new Dictionary<char, string>
+            {
+                ['F'] = F,
+            }, 20, 90);
+            AddPointsToCanvas(l);
+        }
+
+        void SierpinskiTriangle(object sender, RoutedEventArgs e)
+        {
+            const string Axiom = "F-G-G";
+            const string F = "F-G+F+G-F";
+            const string G = "GG";
+            var l = new LSystem(Axiom, new Dictionary<char, string>
+            {
+                ['F'] = F,
+                ['G'] = G,
+            }, 20, 120);
+            AddPointsToCanvas(l);
+        }
+
+        void SierpinskiArrowheadTriangle(object sender, RoutedEventArgs e)
+        {
+            const string Axiom = "A";
+            const string A = "B-A-B";
+            const string B = "A+B+A";
+            var l = new LSystem(Axiom, new Dictionary<char, string>
+            {
+                ['A'] = A,
+                ['B'] = B,
+            }, 20, 60);
+            AddPointsToCanvas(l);
+        }
+    }
+}
